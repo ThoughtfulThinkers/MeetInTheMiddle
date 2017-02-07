55770710ea6438cbf79b190249b499e1a3f78f5b
Nick <nick@itprojmgt.com>
2017-02-07 12:50:18 -0700

    initialize chat

---
+++ envConfig.js
@@ -0,0 +1,7 @@
+export const fbConfig = {
+  apiKey: 'AIzaSyCuVd-6uX1VisVjL17eVtRdAaHA-dV_ITo',
+  authDomain: 'meetup-49273.firebaseapp.com',
+  databaseURL: 'https://meetup-49273.firebaseio.com',
+  storageBucket: 'meetup-49273.appspot.com',
+  messagingSenderId: '612339050346',
+};
---
+++ reducers/ChatReducer.js
@@ -0,0 +1,17 @@
+import {
+  FETCH_CHAT_MESSAGES_SUCCESS
+} from '../actions/types';
+
+const INITIAL_STATE = {
+  messages: ''
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case FETCH_CHAT_MESSAGES_SUCCESS:
+      console.log(action.payload);
+      return { ...state, messages: action.payload };
+    default:
+      return state;
+  }
+};
