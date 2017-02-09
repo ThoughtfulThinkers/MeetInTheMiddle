0db244f3d27b742fee75a451395034f9fd617261
Theresa Wilkin <theresawilkin@yahoo.com>
2017-02-09 12:03:30 -0500

    actions

--- actions/RsvpActions.js
+++ actions/RsvpActions.js
@@ -16,6 +16,26 @@ export const changeRSVP = (street, lat, lon) => {
 export const setRsvp = (street, lat, lon, meetupId) => {
   return (dispatch) => {
     dispatch({ type: SET_RSVP });
-    console.log(street, lat, lon);
+
+    //do firebase auth for current user in real version!
+      //const { currentUser } = firebase.auth();
+    const currentUser = { uid: 'sg6wGGasLIYbmNdzKdZTrc9mPDu1' };
+
+    // const guest = {
+    //
+    // };
+    // firebase.database().ref('/meetups')
+    //   .push(meetup)
+    //   .then(({ key }) => {
+    //     // console.log('created meetup: ', key);
+    //     dispatch({ type: ADD_MEETUP_SUCCESS });
+    //     Actions.meetup({ type: 'reset', meetup });
+    //   })
+    //   .catch((err) => console.log(err));
+    // };
+//    push onto current user meetups
+//    push onto current meetup
+
+    console.log(street, lat, lon, meetupId);
   };
 };
