8c6e58a645c088a9b208dcecf7bf33b7ed53085f
Nick <nick@itprojmgt.com>
2017-02-21 06:11:03 -0700

    tests

--- __tests__/Actions-test.js
+++ __tests__/Actions-test.js
@@ -5,6 +5,21 @@ import * as actions from '../actions';
 /******************************************************
 Auth Actions
 ******************************************************/
+it('inputs EMAIL_CHANGED', () => {
+  expect(actions.emailChanged('user@test.com')).toMatchSnapshot();
+});
+
+it('inputs PASSWORD_CHANGED', () => {
+  expect(actions.passwordChanged('1NewUser')).toMatchSnapshot();
+});
+
+it('inputs AUTH_INPUT_CHANGED', () => {
+  expect(actions.authInputChanged({ prop: 'newPassword', value: 'test' })).toMatchSnapshot();
+});
+
+it('inputs AUTH_INPUT_CHANGED', () => {
+  expect(actions.authInputChanged({ prop: 'confirmPassword', value: 'test' })).toMatchSnapshot();
+});
 
 /******************************************************
 Chat Actions
@@ -58,3 +73,26 @@ RSVP Actions
 /******************************************************
   User Actions
 ******************************************************/
+it('creates a USER_INPUT_CHANGED action', () => {
+  expect(actions.userInputChanged({ prop: 'firstName', value: 'John' })).toMatchSnapshot();
+});
+
+it('creates a USER_INPUT_CHANGED action', () => {
+  expect(actions.userInputChanged({ prop: 'lastName', value: 'Smith' })).toMatchSnapshot();
+});
+
+it('creates a USER_LOCATION_INPUT_CHANGED action', () => {
+  expect(actions.userLocationInputChanged({ prop: 'street', value: '123 Main St' })).toMatchSnapshot();
+});
+
+it('creates a USER_LOCATION_INPUT_CHANGED action', () => {
+  expect(actions.userLocationInputChanged({ prop: 'city', value: 'Salt Lake City' })).toMatchSnapshot();
+});
+
+it('creates a USER_LOCATION_INPUT_CHANGED action', () => {
+  expect(actions.userLocationInputChanged({ prop: 'state', value: 'UT' })).toMatchSnapshot();
+});
+
+it('creates a USER_LOCATION_INPUT_CHANGED action', () => {
+  expect(actions.userLocationInputChanged({ prop: 'zipcode', value: '84111' })).toMatchSnapshot();
+});
